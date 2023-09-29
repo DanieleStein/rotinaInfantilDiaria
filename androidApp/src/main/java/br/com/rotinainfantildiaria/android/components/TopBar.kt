98297a156b2cc3c1bf9828ae49ac51478bed4c3e
Daniele Stein <danieleastein@gmail.com>
2023-09-28 21:42:26 -0300

    Adicionado um alert

---
+++ androidApp/src/main/java/br/com/rotinainfantildiaria/android/components/TopBar.kt
@@ -0,0 +1,19 @@
+package br.com.rotinainfantildiaria.android.components
+
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun TopBar() {
+  TopAppBar() {
+
+  }
+}
+
+@Preview
+@Composable
+fun TopBarPreview() {
+  TopBar()
+}
+
