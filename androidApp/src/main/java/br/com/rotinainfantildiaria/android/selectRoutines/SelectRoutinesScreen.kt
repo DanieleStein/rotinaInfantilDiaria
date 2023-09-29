40152d5c6c0d8ad47b1c99ccb05c67c768e7a550
Daniele Stein <danieleastein@gmail.com>
2023-09-28 21:42:03 -0300

    Adicionando fragments

--- androidApp/src/main/java/br/com/rotinainfantildiaria/android/selectRoutines/SelectRoutinesScreen.kt
+++ androidApp/src/main/java/br/com/rotinainfantildiaria/android/selectRoutines/SelectRoutinesScreen.kt
@@ -2,14 +2,17 @@ package br.com.rotinainfantildiaria.android.selectRoutines
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.com.rotinainfantildiaria.android.MyApplicationTheme
 
 @Composable
@@ -23,14 +26,18 @@ fun SelectRoutinesScreen() {
 
       LazyColumn(
         modifier = Modifier.padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
       ) {
 
         item {
-          OutlinedTextField(value = "Ioodnd", onValueChange = {}, modifier = Modifier.fillMaxWidth())
+          Text(
+            text = "Divirta-se, monte a rotina do seu pequeno!",
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily.Cursive,
+            fontSize = 35.sp,
+            color = Color(0xFFF46799),
+            textAlign = TextAlign.Center
+          )
         }
-
       }
     }
   }
