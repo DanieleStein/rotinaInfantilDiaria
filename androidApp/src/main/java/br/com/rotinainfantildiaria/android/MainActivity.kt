c1ca0fdf6583f87963503952b03e7724cada4e0b
Daniele Stein <danieleastein@gmail.com>
2023-10-03 23:31:33 -0300

    Adicionado a funcao Navigate

--- androidApp/src/main/java/br/com/rotinainfantildiaria/android/MainActivity.kt
+++ androidApp/src/main/java/br/com/rotinainfantildiaria/android/MainActivity.kt
@@ -9,32 +9,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import br.com.rotinainfantildiaria.Greeting
+import br.com.rotinainfantildiaria.android.nav.Navigator
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    GreetingView(Greeting().greet())
-                }
-            }
+          Navigator()
         }
-    }
-}
-
-@Composable
-fun GreetingView(text: String) {
-    Text(text = text)
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        GreetingView("Hello, Android!")
     }
 }
