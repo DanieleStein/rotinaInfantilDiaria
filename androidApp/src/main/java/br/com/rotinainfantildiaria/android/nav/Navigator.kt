a96ca6512a10785fcc32ebdffb7905f0dc3da32d
Daniele Stein <danieleastein@gmail.com>
2023-10-03 23:35:04 -0300

    Criado o pacote para navegaçao no app

---
+++ androidApp/src/main/java/br/com/rotinainfantildiaria/android/nav/Navigator.kt
@@ -0,0 +1,37 @@
+package br.com.rotinainfantildiaria.android.nav
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import br.com.rotinainfantildiaria.android.login.LoginScreen
+import br.com.rotinainfantildiaria.android.selectRoutines.SelectRoutinesScreen
+
+
+enum class Route {
+  LOGIN, HOME
+}
+
+
+@Composable
+fun Navigator(
+  modifier: Modifier = Modifier,
+  navHostController: NavHostController = rememberNavController(),
+  initial: Route = Route.LOGIN
+) {
+  NavHost(navController = navHostController,
+    startDestination = initial.name
+  ) {
+    composable(Route.LOGIN.name) {
+      LoginScreen { navHostController.navigate(Route.HOME.name) }
+    }
+
+    composable(Route.HOME.name) {
+      SelectRoutinesScreen { navHostController.popBackStack() }
+    }
+
+  }
+
+}
