12cee9bb39a93661278f82ef8f275fc458df6d32
Daniele Stein <danieleastein@gmail.com>
2023-10-30 23:29:21 -0300

    Nova tela para selecao do gender

---
+++ androidApp/src/main/java/br/com/rotinainfantildiaria/android/selectGender/SelectGenderScreen.kt
@@ -0,0 +1,16 @@
+package br.com.rotinainfantildiaria.android.selectGender
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SelectGenderScreen(gender: String) {
+
+
+}
+
+@Preview
+@Composable
+fun SelectGenderScreenPreview() {
+  SelectGenderScreen(gender = )
+}
