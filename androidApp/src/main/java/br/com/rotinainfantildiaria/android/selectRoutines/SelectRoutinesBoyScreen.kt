d97189eef09a7a087e5383505f1e6f2e8e80be28
Daniele Stein <danieleastein@gmail.com>
2023-11-09 15:09:21 -0300

    Criada a tela SelectRoutinesBoyScreen

---
+++ androidApp/src/main/java/br/com/rotinainfantildiaria/android/selectRoutines/SelectRoutinesBoyScreen.kt
@@ -0,0 +1,16 @@
+package br.com.rotinainfantildiaria.android.selectRoutines
+
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import br.com.rotinainfantildiaria.android.MyApplicationTheme
+
+@Composable
+fun SelectRoutinesBoyScreen() {
+  MyApplicationTheme {
+    Scaffold {
+      
+    }
+
+  }
+
+}
