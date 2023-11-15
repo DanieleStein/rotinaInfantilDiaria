617ae5d73bea6fb170fdccf1056c4cc38dfebb50
Daniele Stein <danieleastein@gmail.com>
2023-11-14 23:36:39 -0300

    Novo TopBar

--- androidApp/src/main/java/br/com/rotinainfantildiaria/android/components/TopBar.kt
+++ androidApp/src/main/java/br/com/rotinainfantildiaria/android/components/TopBar.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
@@ -17,11 +18,12 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import br.com.rotinainfantildiaria.android.R
+import br.com.rotinainfantildiaria.android.ui.theme.green
 import br.com.rotinainfantildiaria.android.ui.theme.pinkWriting
 import br.com.rotinainfantildiaria.android.ui.theme.yellow
 
 @Composable
-fun TopBarSelect(title: String) {
+fun TopBarGirl(title: String) {
   TopAppBar(
     backgroundColor = yellow,
     /*navigationIcon = {
@@ -55,7 +57,7 @@ fun TopBarSelect(title: String) {
 }
 
 @Composable
-fun BottomAppBarSelect(title: String, onClick: () -> Unit) {
+fun BottomAppBarGirl(title: String, onClick: () -> Unit) {
   BottomAppBar(
     backgroundColor = yellow
   ) {
@@ -75,24 +77,7 @@ fun BottomAppBarSelect(title: String, onClick: () -> Unit) {
 }
 
 @Composable
-fun TopBarList(title: String) {
-  TopAppBar(
-    backgroundColor = yellow
-  ) {
-    Text(
-      text = title,
-      color = (pinkWriting),
-      fontSize = 30.sp,
-      fontWeight = FontWeight.Bold,
-      fontFamily = FontFamily.Cursive,
-      modifier = Modifier.fillMaxWidth(),
-      textAlign = TextAlign.Center
-    )
-  }
-}
-
-@Composable
-fun FloatingButtonList(onClick: () -> Unit) {
+fun FloatingButtonGirl(onClick: () -> Unit) {
   FloatingActionButton(
     onClick = onClick,
     backgroundColor = yellow,
@@ -106,28 +91,109 @@ fun FloatingButtonList(onClick: () -> Unit) {
   }
 }
 
+@Composable
+fun TopBarBoy (title: String) {
+  TopAppBar(
+    backgroundColor = green,
+    /*navigationIcon = {
+      IconButton(onClick = { /*TODO*/ }) {
+        Icon(Icons.Filled.Menu, "Menu", tint = pinkWriting)
+      }
+    }*/
+    title = {
+      Text(
+        text = title,
+        color = Color.White,
+        fontFamily = FontFamily.Cursive,
+        fontWeight = FontWeight.Bold,
+        fontSize = 30.sp,
+        modifier = Modifier.fillMaxWidth(),
+        textAlign = TextAlign.Center
+      )
+    },
+    actions = {
+      IconButton(onClick = { /*TODO*/ }) {
+        Image(
+          imageVector = ImageVector.vectorResource(R.drawable.ic_profile),
+          contentDescription = "profile",
+          contentScale = ContentScale.FillWidth,
+          modifier = Modifier.size(30.dp),
+          colorFilter = ColorFilter.tint(color = Color.White)
+        )
+      }
+    }
+  )
+}
+
+@Composable
+fun FloatingButtonBoy(onClick: () -> Unit) {
+  FloatingActionButton(
+    onClick = onClick,
+    backgroundColor = green,
+  ) {
+    Image(
+      imageVector = ImageVector.vectorResource(R.drawable.ic_add),
+      contentDescription = "adicionar",
+      modifier = Modifier.size(40.dp),
+      colorFilter = ColorFilter.tint(Color.White)
+    )
+  }
+}
+
+@Composable
+fun BottomAppBarBoy(title: String, onClick: () -> Unit) {
+  BottomAppBar(
+    backgroundColor = green
+  ) {
+    TextButton(onClick = onClick) {
+      Text(
+        text = title,
+        color = Color.White,
+        fontFamily = FontFamily.Cursive,
+        fontWeight = FontWeight.Bold,
+        fontSize = 30.sp,
+        modifier = Modifier.fillMaxWidth(),
+        textAlign = TextAlign.End
+
+      )
+    }
+  }
+}
+
 
 @Preview
 @Composable
-fun TopBarSelectPreview() {
-  TopBarSelect("Routine")
+fun TopBarGirlPreview() {
+  TopBarGirl("Routine")
 }
 
 @Preview
 @Composable
-fun BottomAppBarSelectPreview() {
-  BottomAppBarSelect("Confirmar", onClick = {})
+fun BottomAppBarGirltPreview() {
+  BottomAppBarGirl("Confirmar", onClick = {})
 }
 
 @Preview
 @Composable
-fun TopBarListPreview() {
-  TopBarList(title = "Routine")
+fun FloatingButtonGirlPreview() {
+  FloatingButtonGirl {}
 }
 
 @Preview
 @Composable
-fun FloatingButtonPreview() {
-  FloatingButtonList {}
+fun TopBarBoyPreview() {
+  TopBarBoy(title = "Routine")
+}
+
+@Preview
+@Composable
+fun BottomAppBarBoytPreview() {
+  BottomAppBarBoy("Confirmar", onClick = {})
+}
+
+@Preview
+@Composable
+fun FloatingButtonBoyPreview() {
+  FloatingButtonBoy {}
 }
 
