7762ffcc96eff7fa150e0ea44fc56f0fcb988b41
Daniele Stein <danieleastein@gmail.com>
2023-11-29 23:07:10 -0300

    Criada novas cores no app

--- androidApp/src/main/java/br/com/rotinainfantildiaria/android/ui/theme/Color.kt
+++ androidApp/src/main/java/br/com/rotinainfantildiaria/android/ui/theme/Color.kt
@@ -13,4 +13,9 @@ val pinkSelected = Color(0xFFF46799)
 val pinkDisabled = Color(0xFFA3395F)
 val green = Color(0xFF62c780)
 val blueList = Color(0xFFB0E0F5)
+val violet = Color(0xFF845EC2)
+val pink = Color(0x80D65DB1)
+val blue = Color(0x802C73D2)
+
+
 
