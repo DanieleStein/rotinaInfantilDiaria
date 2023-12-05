2923d6b49b568b5db1f21785ae8527aaa2604baf
Daniele Stein <danieleastein@gmail.com>
2023-12-04 23:48:40 -0300

    Remocao de cor

--- androidApp/src/main/java/br/com/rotinainfantildiaria/android/ui/theme/Color.kt
+++ androidApp/src/main/java/br/com/rotinainfantildiaria/android/ui/theme/Color.kt
@@ -5,8 +5,6 @@ import androidx.compose.ui.graphics.Color
 val yellow = Color(0xFFFDE9AA)
 val pinkWriting = Color(0xFFF46799)
 val pinkBackground = Color(0xFFFFEDED)
-val white = Color(0xFFFFFFFF)
-val black = Color(0xFF000000)
 val blueSelected = Color(0xFF67c7f2)
 val blueDisabled = Color(0xFF247296)
 val pinkSelected = Color(0xFFF46799)
